9b9652fe7906de68235cacc93d0a45522a8f63a6
Arjunꦗ꦳ꦻꦤ꧀ <arjunzain225@gmail.com>
2025-10-13 21:51:51 +0700

    Create Latihan05.java

---
+++ JobSheet1/Latihan05.java
@@ -0,0 +1,17 @@
+public class Latihan05 {
+    {
+public static void main (String[] args)
+        System.out.println("NIM\tNAMA\tPRODI");
+
+        System.out.println("030\tWildan\tTeknik Informatika");
+        System.out.println("007\tNaura\tTeknik Informatika");
+        System.out.println("197\tWarda\tTeknik Informatika");
+        System.out.println("032\tBagas\tTeknik Informatika");
+        System.out.println("213\tTegar\tTeknik Informatika");
+        System.out.println("152\tDito\tTeknik Informatika");
+        System.out.println("098\tDio\tTeknik Informatika");
+        System.out.println("078\tAril\tTeknik Informatika");
+        System.out.println("198\tFajri\tTeknik Informatika");
+        System.out.println("056\tAlfa\tTeknik Informatika");
+    }
+}
