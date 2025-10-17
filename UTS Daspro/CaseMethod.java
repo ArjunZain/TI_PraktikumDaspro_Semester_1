2d4c29f6f25d438043f1c25e0f4173c0cf86c54f
Arjunꦗ꦳ꦻꦤ꧀ <arjunzain225@gmail.com>
2025-10-17 08:47:15 +0700

    Update CaseMethod.java

--- UTS Daspro/CaseMethod.java
+++ UTS Daspro/CaseMethod.java
@@ -1,1 +1,23 @@
+public class Ab {
+    public static void main(String[] args) {
 
+        System.out.println("Halo, Nama saya adalah XXXX!");
+        System.out.println("Belajar java itu mudah!");
+        System.out.println("Semakin sering mencoba, semakin jago.");
+
+        System.out.println("NIM\t\tNama\t\tProdi");
+        System.out.println("010\t\tArjun\t\tTI");
+
+        String salah_satu_hoby_saya_adalah = "olahraga";
+        boolean isPandai =true;
+        char jenisKelamin ='L';
+        byte umurSayasekarang =20;
+        double ipk =3.78, tinggi =1.71;
+
+        System.out.println(salah_satu_hoby_saya_adalah);
+        System.out.println("Apakah saya pandai? " +isPandai);
+        System.out.println("Jenis kelamin saya Adalah" +jenisKelamin);
+        System.out.println("Umur saya sekarang" +umurSayasekarang);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", ipk, tinggi));
+    }
+}
